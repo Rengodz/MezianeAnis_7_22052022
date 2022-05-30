579ecc7b76b6190e99caf3f793118814114a3b9f
Anis <mrmezianea@gmail.com>
2022-05-30 07:48:29 +0200

    first commit

---
+++ front/frontApp/src/components/background.vue
@@ -0,0 +1,22 @@
+<template>
+ <div :style="image"></div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      image: { backgroundImage: "url(./src/assets/groupobk.png)" }
+    };
+  }
+};
+</script>
+
+<style>
+
+.image{
+   color:linear-gradient(rgba(0, 0, 255, 0.5), rgba(255, 255, 0, 0.5))
+}
+
+</style>
