886895b4d76e8780f5c681c3c6e11779c33b9130
Anis <mrmezianea@gmail.com>
2022-06-13 21:18:59 +0200

    sql-to-mongoose

---
+++ back/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+require('dotenv').config({ path: './config/.env' });
+const mongoose = require('mongoose');
+const path = require('path');
+
+const userRoutes = require('./routes/user');
+
+mongoose.connect(process.env.DB_CONFIG, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+.then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
--- back/models/users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE user (
-id_user int NOT NULL,
-Nom text,
-Mot_de_passe password,
-PRIMARY KEY (id_user)
-)
